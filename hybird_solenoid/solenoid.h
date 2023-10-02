3c8d8f2063942a762dc3d3e0c079ebb9e0930fda
niekky <trungkiencfc@gmail.com>
2023-10-01 23:17:12 -0700

    Initialize solenoid codes for hybrid project

---
+++ hybird_solenoid/solenoid.h
@@ -0,0 +1,62 @@
+/*******************************************************************************
+*
+* FILE:
+* 		solenoid.h
+*
+* DESCRIPTION:
+* 		Basic solenoid actuation API for hyprid engine
+*
+*******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef SOLENOID_H
+#define SOLENOID_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*------------------------------------------------------------------------------
+ Includes 
+------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+ Macros 
+------------------------------------------------------------------------------*/
+
+
+
+
+/*------------------------------------------------------------------------------
+ Types                                                                     
+------------------------------------------------------------------------------*/
+
+/* Execute a solenoid command */
+void solenoid_cmd_execute
+	(
+	uint8_t solenoid_cmd_opcode  /* Solenoid actuation code */
+	); /* solenoid_cmd_execute */
+
+/* Turn a solenoid on */
+void solenoid_on
+	(
+	uint8_t solenoid_num  /* Solenoid number to actuate */
+	); /* solenoid_on */
+
+/* Turn a solenoid off */
+void solenoid_off
+	(
+	uint8_t solenoid_num  /* Solenoid number to actuate */
+	); /* solenoid_off */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SOLENOID_H */
+
+/*******************************************************************************
+* END OF FILE                                                                  *
+*******************************************************************************/
