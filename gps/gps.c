c3196346db8c8c472f62705b0e0014326efd13cb
niekky <trungkiencfc@gmail.com>
2023-11-24 13:37:37 -0700

    UART4 Initialization for GPS

---
+++ gps/gps.c
@@ -0,0 +1,156 @@
+/*******************************************************************************
+*
+* FILE: 
+* 		gps.c
+*
+* DESCRIPTION: 
+* 		Contains API functions for GPS 
+*
+*******************************************************************************/
+
+
+/*------------------------------------------------------------------------------
+ Standard Includes  
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+ MCU Pins 
+------------------------------------------------------------------------------*/
+#if   defined( FLIGHT_COMPUTER      )
+	#include "sdr_pin_defines_A0002.h"
+#elif defined( ENGINE_CONTROLLER    )
+	#include "sdr_pin_defines_L0002.h"
+#elif defined( VALVE_CONTROLLER     )
+	#include "sdr_pin_defines_L0005.h"
+#elif defined( GROUND_STATION       )
+	#include "sdr_pin_defines_A0005.h"
+#elif defined( FLIGHT_COMPUTER_LITE )
+	#include "sdr_pin_defines_A0007.h"
+#endif
+
+
+/*------------------------------------------------------------------------------
+ Project Includes                                                               
+------------------------------------------------------------------------------*/
+#include "main.h"
+#include "gps.h"
+
+
+/*------------------------------------------------------------------------------
+ Preprocesor Directives 
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+Global Variables                                                                  
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+ Procedures 
+------------------------------------------------------------------------------*/
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
+* 		gps_transmit                                                    *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+* 		transmits a specified number of bytes over USB                         *
+*                                                                              *
+*******************************************************************************/
+GPS_STATUS gps_transmit 
+	(
+    uint8_t*    tx_data_ptr , /* Data to be sent       */	
+	size_t   tx_data_size, /* Size of transmit data */ 
+	uint32_t timeout       /* UART timeout          */
+	)
+{
+/*------------------------------------------------------------------------------
+ Local Variables
+------------------------------------------------------------------------------*/
+HAL_StatusTypeDef gps_status;
+
+
+/*------------------------------------------------------------------------------
+ API Function Implementation 
+------------------------------------------------------------------------------*/
+
+/* Transmit byte */
+gps_status = HAL_UART_Transmit( &( GPS_HUART ),
+                                tx_data_ptr   , 
+                                tx_data_size  , 
+                                timeout );
+
+/* Return HAL status */
+if ( gps_status != HAL_OK )
+	{
+	return gps_status;
+	}
+else
+	{
+	return GPS_OK;
+	}
+
+} /* usb_transmit */
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   *
+* 		usb_recieve                                                           *
+*                                                                              *
+* DESCRIPTION:                                                                 *
+* 	    Receives bytes from the USB port                                       *
+*                                                                              *
+*******************************************************************************/
+GPS_STATUS gps_receive 
+	(
+	uint8_t*    rx_data_ptr , /* Buffer to export data to        */
+	size_t   rx_data_size, /* Size of the data to be received */
+	uint32_t timeout       /* UART timeout */
+	)
+{
+/*------------------------------------------------------------------------------
+ Local Variables
+------------------------------------------------------------------------------*/
+HAL_StatusTypeDef gps_status;
+
+
+/*------------------------------------------------------------------------------
+ API Function Implementation 
+------------------------------------------------------------------------------*/
+
+/* Transmit byte */
+gps_status = HAL_UART_Receive( &( GPS_HUART ),
+                               rx_data_ptr   , 
+                               rx_data_size  , 
+                               timeout );
+
+/* Return HAL status */
+switch ( gps_status )
+	{
+	case HAL_TIMEOUT:
+		{
+		return GPS_TIMEOUT;
+		break;
+		}
+	case HAL_OK:
+		{
+		return GPS_OK;
+		break;
+		}
+	default:
+		{
+		return GPS_FAIL;
+		break;
+        }
+	}
+
+} /* usb_receive */
+
+/*******************************************************************************
+* END OF FILE                                                                  * 
+*******************************************************************************/
---
+++ gps/gps.h
@@ -0,0 +1,67 @@
+/*******************************************************************************
+*
+* FILE: 
+* 		gps.h
+*
+* DESCRIPTION: 
+* 		Contains API functions for gps
+*
+*******************************************************************************/
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef GPS_H
+#define GPS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*------------------------------------------------------------------------------
+ Includes 
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+ Typdefs 
+------------------------------------------------------------------------------*/
+
+/* Function return codes */
+typedef enum GPS_STATUS
+	{
+	GPS_OK = 0,
+    GPS_FAIL  ,
+	GPS_TIMEOUT
+	} GPS_STATUS;
+
+
+/*------------------------------------------------------------------------------
+ Function Prototypes 
+------------------------------------------------------------------------------*/
+
+/* transmits bytes over USB */
+GPS_STATUS gps_transmit 
+	(
+    uint8_t*    tx_data_ptr , /* Data to be sent       */	
+	size_t   tx_data_size, /* Size of transmit data */ 
+	uint32_t timeout       /* UART timeout          */
+	);
+
+/* Receives bytes from the USB port */
+GPS_STATUS gps_receive 
+	(
+	uint8_t*    rx_data_ptr , /* Buffer to export data to        */
+	size_t   rx_data_size, /* Size of the data to be received */
+	uint32_t timeout       /* UART timeout */
+	);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* USB_H */
+
+/*******************************************************************************
+* END OF FILE                                                                  * 
+*******************************************************************************/
