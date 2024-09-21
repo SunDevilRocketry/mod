ea5cf9ff829f36b32536ca9f09fe8a974f963d3c
Dexx Stexx <drewpad@icloud.com>
2024-09-21 12:42:58 -0700

    Create helper functions and port set_lora_chip_mode to them

--- lora/lora.c
+++ lora/lora.c
@@ -26,10 +26,31 @@
 #endif
 
 /*------------------------------------------------------------------------------
+ Helper functions for various pin functions on the LoRa modem.
+------------------------------------------------------------------------------*/
+void lora_write_cs_pin( GPIO_PinState pinState ) {
+    /* Takes either GPIO_PIN_SET or GPIO_PIN_RESET to bring the chip select pin
+       high or low.*/
+    HAL_GPIO_WritePin( LORA_SS_GPIO_PORT, pinState );
+}
+
+HAL_StatusTypeDef lora_spi_receive( uint8_t *register ) {
+    /* Takes pointer to the requested register. The response size is set to
+       1 byte, as that is the size of the LoRa module's registers*/
+    return HAL_SPI_Receive( &(LORA_SPI), &(register)), 1, HAL_DEFAULT_TIMEOUT  );
+}
+
+HAL_StatusTypeDef lora_spi_transmit( uint8_t register, uint8_t data ) {
+    /* Takes register and data to write (1 byte) and writes that register. */
+    uint8_t transmitBuffer[2] = { register, data };
+    HAL_SPI_Transmit( &(LORA_SPI),  &transmitBuffer, 2, HAL_DEFAULT_TIMEOUT);
+}
+
+/*------------------------------------------------------------------------------
  Function to set the mode of the chip
 ------------------------------------------------------------------------------*/
 
-void set_chip_mode( uint8_t chip_mode ) {
+void set_lora_chip_mode( uint8_t chip_mode ) {
     /* chip_mode should be one of the following 3-bit variables defined in lora.h
             #define LORA_SLEEP_MODE            0b000
             #define LORA_STANDBY_MODE          0b001
@@ -43,10 +64,10 @@ void set_chip_mode( uint8_t chip_mode ) {
     // Also, may need to be updated based on pin definitions after those are fixed.
 
     // Pull chip select low
-    HAL_GPIO_WritePin( LORA_SS_GPIO_PORT, LOSS_SS_GPIO_PIN, GPIO_PIN_RESET );
+    lora_write_cs_pin( GPIO_PIN_RESET );
 
     // Get initial value of the operation mode register
-    HAL_StatusTypeDef operation_mode_register = HAL_SPI_Receive( &(LORA_SPI), &(LORA_REG_OPERATION_MODE), 1, HAL_DEFAULT_TIMEOUT  );
+    HAL_StatusTypeDef operation_mode_register = lora_spi_receive( &(LORA_REG_OPERATION_MODE) );
 
     // Shift the chip mode bits to be the first 3 bits of the sequence
     uint8_t shifted_chip_mode = (chip_mode << 0b00000);
@@ -55,8 +76,8 @@ void set_chip_mode( uint8_t chip_mode ) {
     uint8_t new_opmode_register = (operation_mode_register | chip_mode);
 
     // Write new bit
-    HAL_SPI_Transmit( &(LORA_SPI),  &new_opmode_register, 1, HAL_DEFAULT_TIMEOUT);
+    lora_spi_transmit( LORA_REG_OPERATION_MODE, new_opmode_register );
 
     // Bring chip select back high
-    HAL_GPIO_WritePin( LORA_SS_GPIO_PORT, LOSS_SS_GPIO_PIN, GPIO_PIN_SET );
+    lora_write_cs_pin( GPIO_PIN_RESET );
 }
