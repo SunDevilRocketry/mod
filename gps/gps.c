3f4082216b3da2b0999b03625e280352dd84dbd9
NArmistead <nicholas.armistead05@gmail.com>
2026-02-20 10:18:01 -0700

    Make GPS parse work with emulator

--- gps/gps.c
+++ gps/gps.c
@@ -283,7 +283,7 @@ void GPS_parse(GPS_DATA* gps_ptr, char *GPSstrParse){
 /* Get message type */
 char token[8]; // Needs to be 8 chars for memory alignment
 strncpy(token, GPSstrParse, 6);
-token[7] = '\0';
+token[6] = '\0';
 int idx = 7; /* Skips "$GPXXX,"*/
 
 /* Parse by message type */
