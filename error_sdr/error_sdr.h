eaf186dfbb61ddf9d1621717bc0a9b259f480de0
ETSound <etsells44@gmail.com>
2026-05-23 13:39:18 -0700

    Make the macro safer without binding to else branches

--- error_sdr/error_sdr.h
+++ error_sdr/error_sdr.h
@@ -140,7 +140,7 @@ typedef struct TEXT_MESSAGE
 * 		Checks condition, if false calls error_fail_fast with error            *
 *                                                                              *
 *******************************************************************************/
-#define assert_fail_fast( condition, error ) if ( !(condition) ) error_fail_fast( error )
+#define assert_fail_fast( condition, error ) do { if ( !(condition) ) error_fail_fast( error ); } while(0)
 
 
 /*******************************************************************************
@@ -154,7 +154,7 @@ typedef struct TEXT_MESSAGE
 *                                                                              *
 *******************************************************************************/
 #define debug_assert( condition, error ) \
-if ( !(condition) && ( defined(DEBUG) || !defined(RELBLD) ) ) error_fail_fast( error )
+do { if ( !(condition) && ( defined(DEBUG) || !defined(RELBLD) ) ) error_fail_fast( error ); } while(0)
 
 
 /*------------------------------------------------------------------------------
