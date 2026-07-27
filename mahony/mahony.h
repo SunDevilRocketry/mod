acab7f4280f8d8a3f232f59baf50da9f104ea33a
Bjorn Bengtsson <bjorn.h.bengtsson@gmail.com>
2026-07-26 19:13:51 -0700

    Move shared vector type to math module

--- mahony/mahony.h
+++ mahony/mahony.h
@@ -31,16 +31,6 @@ extern "C"
  ------------------------------------------------------------------------------*/
 
 /**
- * @brief Three-dimensional floating-point vector.
- */
-typedef struct _VECTOR_3F
-    {
-    float x;
-    float y;
-    float z;
-    } VECTOR_3F;
-
-/**
  * @brief State and gains for a Mahony attitude filter.
  */
 typedef struct _MAHONY_FILTER
