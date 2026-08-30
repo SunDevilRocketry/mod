a179c7de5d78d4c6617fe0540462d83c4295c25a
NArmistead <nicholas.armistead05@gmail.com>
2026-08-30 13:56:48 -0700

    Create kalman header

---
+++ kalman/kalman.h
@@ -0,0 +1,39 @@
+/**
+  ******************************************************************************
+  * @file           : halman.h
+  * @brief          : Kalman filter for altitude, vertical velocity, and
+  *                   vertical acceleration.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 Sun Devil Rocketry.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is covered under the   
+  * BSD-3-Clause.                                                          
+  *                                                                              
+  * https://opensource.org/license/bsd-3-clause
+  *
+  ******************************************************************************
+  @verbatim
+  ==============================================================================
+                      ##### Kalman filter features #####
+  ==============================================================================
+  [..]
+  (+) todo
+  ******************************************************************************
+  @endverbatim
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef KALMAN_H 
+#define KALMAN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "kalman_init_types.h"  /* Includes the KALMAN_OUTPUT struct for step returns */
+#include "kalman_init.h"        /* Includes one-time init function */
+#include "kalman_step.h"        /* Includes step function */
+
+#endif /* KALMAN_H */
